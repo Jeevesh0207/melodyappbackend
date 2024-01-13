e4eda90b747414027175edadaf5236d96eec9d33
Jeevesh0207 <r27abbit@gmail.com>
2024-01-13 23:02:27 +0530

    Add Search

---
+++ routes/Search.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const express = require('express');
+const Search = express.Router();
+
+Search.get('/search/:parameter', async (req, res) => {
+    try {
+        const name = req.params.parameter;
+        // console.log(name)
+        const url = `https://saavn.me/search/songs?query=${name}&page=1&limit=20`;
+        let cancelToken = null;
+        if (cancelToken) {
+            cancelToken.cancel('Operations cancelled due to new request');
+        }
+        cancelToken = axios.CancelToken.source();
+
+        const response = await axios.get(url);
+
+        const data = response.data;
+        const allData = data.data.results;
+
+        const array = allData.map((item, index) => {
+            const itemName = item.name;
+            const itemArtist = item.primaryArtists;
+            const imgLen = item.image.length;
+            const imgUrl = item.image[imgLen - 1].link;
+            const songLen = item.downloadUrl.length;
+            const songUrl = item.downloadUrl[songLen - 1].link;
+            const uniqueId=item.id
+            return {
+                id: index,
+                Name: itemName,
+                Artist: itemArtist,
+                YTID: "",
+                Url: imgUrl,
+                SongUrl: songUrl,
+                uniqueId:uniqueId
+            };
+        });
+
+        res.send(array);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+module.exports = Search;
--- routes/routes.js
+++ routes/routes.js
@@ -5,11 +5,13 @@ const OtpGenerate=require("./OtpGenerate")
 const SignIn = require("./SignIn")
 const SongFetch = require("./SongFetch")
 const SongData = require("./SongData")
+const Search = require("./Search")
 
 router.use('/',SignUp)
 router.use('/',OtpGenerate)
 router.use('/',SignIn)
 router.use('/',SongFetch)
 router.use('/',SongData)
+router.use('/',Search)
 
 module.exports=router
