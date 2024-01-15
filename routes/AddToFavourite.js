29a2d0202cd1f8ceaf57cf2b41eb4576cf88c5fd
Jeevesh0207 <r27abbit@gmail.com>
2024-01-15 08:20:55 +0530

    Testing 12

--- routes/AddToFavourite.js
+++ routes/AddToFavourite.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const AddToFavourite = express.Router()
-const Schema = require('../model/Schemas')
+const Schema=require("../model/Schemas")
 
 AddToFavourite.post('/addtofavorite', async (req, res) => {
     const UserName = req.body.UserName
@@ -11,36 +11,37 @@ AddToFavourite.post('/addtofavorite', async (req, res) => {
     const YTID = req.body.YTID
     const uniqueId = req.body.uniqueId
     const UserData = Schema.UsersData
-    const UserFavData = await UserData.find({ UserName: UserName })
-    console.log(UserName,UserFavData)
-    const SongsArray = UserFavData.SongData
-
-    if (SongsArray.some(item => item.Name === Name)) {
-        const PullData = await UserData.updateOne({ UserName: UserName }, {
-            $pull: {
-                SongData: {
-                    Name: Name
+    const UserFavData = await UserData.findOne({ UserName: UserName })
+    console.log(UserName, UserFavData)
+    if (UserFavData) {
+        const SongsArray = UserFavData.SongData || [];
+        if (SongsArray.some(item => item.Name === Name)) {
+            const PullData = await UserData.updateOne({ UserName: UserName }, {
+                $pull: {
+                    SongData: {
+                        Name: Name
+                    }
                 }
+            })
+            if (PullData) {
+                res.send("Data Deleted")
             }
-        })
-        if (PullData) {
-            res.send("Data Deleted")
-        }
-    } else {
-        const AddData = await UserData.updateOne({ UserName: UserName }, {
-            $push: {
-                SongData: {
-                    Name: Name,
-                    Artist: Artist,
-                    Url: Url,
-                    SongUrl: SongUrl,
-                    YTID: YTID,
-                    uniqueId: uniqueId
+        } else {
+            const AddData = await UserData.updateOne({ UserName: UserName }, {
+                $push: {
+                    SongData: {
+                        Name: Name,
+                        Artist: Artist,
+                        Url: Url,
+                        SongUrl: SongUrl,
+                        YTID: YTID,
+                        uniqueId: uniqueId
+                    }
                 }
+            })
+            if (AddData) {
+                res.send("Data Added")
             }
-        })
-        if (AddData) {
-            res.send("Data Added")
         }
     }
     res.end()
@@ -48,11 +49,9 @@ AddToFavourite.post('/addtofavorite', async (req, res) => {
 
 AddToFavourite.post('/addtofavorite/find', async (req, res) => {
     const UserName = req.body.UserName
-    const YTID = req.body.YTID
-    const uniqueId = req.body.uniqueId
     const Name = req.body.Name
     const UserData = Schema.UsersData
-    const UserFavData = await UserData.find({ UserName: UserName })
+    const UserFavData = await UserData.findOne({ UserName: UserName })
     const SongsArray = UserFavData.SongData
     if (SongsArray !== undefined) {
         if (SongsArray.some(item => item.Name === Name)) {
@@ -75,16 +74,16 @@ AddToFavourite.post('/addtofavorite/find', async (req, res) => {
 AddToFavourite.post('/addtofavorite/data', async (req, res) => {
 
     const UserName = req.body.UserName
-    const Data = Schema.UsersData
-    const UserFavData=await Data.findOne({UserName:UserName})
-    console.log(UserName,UserFavData )
+    const UsersData = Schema.UsersData
+    const UserFavData = await UsersData.findOne({ UserName: UserName })
+    console.log(UserName, UserFavData)
     if (UserFavData && UserFavData.SongData) {
         const SongsArray = UserFavData.SongData
         res.send(SongsArray)
-    }else{
+    } else {
         res.send("Error")
     }
-    
+
 
 });
 
